2143393ed6900688d5b59a6dfea3f3239375398d
nazarperkatyuk <perkatyuk.n@gmail.com>
2020-03-21 20:14:59 +0200

    add home(servlet)

--- src/main/java/home4/LoginServlet.java
+++ src/main/java/home4/LoginServlet.java
@@ -9,34 +9,4 @@ import java.util.Optional;
 
 public class LoginServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("login.jsp").forward(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        String email=req.getParameter("login");
-        String password = req.getParameter("password");
-
-        User_DAO user_dao = new User_DAO();
-
-        Optional<User> user = user_dao.getUserByEmail(email);
-
-        if (!user.isPresent()){
-            req.getRequestDispatcher("login.jsp").forward(req, resp);
-            return;
-        }
-
-        User users = user.get();
-
-        if (users.getPassword().equals(password)){
-            req.setAttribute("firstName", users.getFirstName());
-            req.setAttribute("lastName", users.getLastName());
-            req.getRequestDispatcher("cabinet.jsp").forward(req, resp);
-        }
-
-        req.getRequestDispatcher("login.jsp").forward(req, resp);
-    }
-}
+ 
